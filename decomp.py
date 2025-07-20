e60ae98fb404e5d0b1259346a5e272d0a505c37b
Tilden <dev@tilden.me>
2025-07-19 23:49:11 -0400

    Add parse out memory changes and printing

--- decomp.py
+++ decomp.py
@@ -3,7 +3,7 @@
 # @assumes: no user input -- no ","
 
 import sys
-import re
+import string
 
 filename = sys.argv[1]
 with open(filename, "r") as file:
@@ -13,21 +13,29 @@ def add_command(program, cmd_str, indentation):
     program += "\t"*indentation + cmd_str + "\n"
     return program
 
-#try more of a simulator
-result = ""
+def check_printable(value):
+    if chr(value) in string.printable:
+        return repr(chr(value))
+    else:
+        return str(value)
 
+#try more of a simulator
 memory = [0]*30_000
 ptr = 0
 while_stack = []
+
+decompiled = """memory = [0]*30_000\n"""
+changes = [0]*30_000
+
 i = 0
 while i < len(code):
     cmd = code[i]
     match cmd:
         case "+":
-            memory[ptr] += 1
+            changes[ptr] += 1
             
         case "-":
-            memory[ptr] -= 1
+            changes[ptr] -= 1
             
         case ">":
             ptr += 1
@@ -40,7 +48,7 @@ while i < len(code):
             
         case "]":
             last = while_stack.pop()
-            if (memory[ptr] != 0):
+            if (memory[ptr] + changes[ptr] != 0):
                 i = last
                 continue
 
@@ -48,8 +56,15 @@ while i < len(code):
             pass
 
         case ".":
-            result += chr(memory[ptr])
+            for j in range(len(changes)):
+                memory[j] += changes[j]
+
+                if (changes[j] != 0):
+                    decompiled = add_command(decompiled, f"memory[{j}] = {check_printable(memory[j])}", 0)
+                    changes[j] = 0
+            
+            decompiled = add_command(decompiled, f"print({check_printable(memory[ptr])})", 0)
             
     i+=1
 
-print(result)
+print(decompiled)
